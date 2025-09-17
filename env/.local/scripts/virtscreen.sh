e8e0c42da06a1d01e86dfc3048e58a7e3289b222
sowsantiheig <santiago.sugranes@heig-vd.ch>
2025-09-17 19:33:27 +0200

    feat: helpful virt screen
    mainly used if I want to record something on a different screen while
    working on my main one but don't have a physical screen

---
+++ env/.local/scripts/virtscreen.sh
@@ -0,0 +1,7 @@
+cvt 1024 768 60
+xrandr --newmode "1024x768_60.00"   63.50  1024 1072 1176 1328  768 771 775 798 -hsync +vsync
+xrandr --addmode DP-1 1024x768_60.00
+xrandr --output DP-1 --mode 1024x768_60.00 --right-of eDP-1
+
+cvt 1280 720 30
+cvt 1920 1080 10
