c2c3e1615d134891e75ec58e1b0e9d4bc7a4fd5f
Santiago Sugrañes <santiago.sugranes@heig-vd.ch>
2025-09-25 09:27:48 +0200

    formatting + so3 debug+compile helpers

--- env/.config/nvim/lua/plugins/debug.lua
+++ env/.config/nvim/lua/plugins/debug.lua
@@ -1,8 +1,8 @@
 vim.pack.add({
-	{ src = 'https://github.com/nvim-neotest/nvim-nio' },
-	{ src = 'https://github.com/mfussenegger/nvim-dap' },
-	{ src = 'https://github.com/rcarriga/nvim-dap-ui' },
-	{ src = 'https://github.com/theHamsta/nvim-dap-virtual-text' }
+    { src = 'https://github.com/nvim-neotest/nvim-nio' },
+    { src = 'https://github.com/mfussenegger/nvim-dap' },
+    { src = 'https://github.com/rcarriga/nvim-dap-ui' },
+    { src = 'https://github.com/theHamsta/nvim-dap-virtual-text' }
 })
 
 local dap = require "dap"
@@ -11,65 +11,93 @@ local ui = require "dapui"
 require("dapui").setup()
 
 require("nvim-dap-virtual-text").setup {
-	-- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
-	display_callback = function(variable)
-		local name = string.lower(variable.name)
-		local value = string.lower(variable.value)
-		if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
-			return "*****"
-		end
-
-		if #variable.value > 15 then
-			return " " .. string.sub(variable.value, 1, 15) .. "... "
-		end
-
-		return " " .. variable.value
-	end,
+    display_callback = function(variable)
+        local name = string.lower(variable.name)
+        local value = string.lower(variable.value)
+        if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+            return "*****"
+        end
+        if #variable.value > 15 then
+            return " " .. string.sub(variable.value, 1, 15) .. "... "
+        end
+        return " " .. variable.value
+    end,
 }
 
-
-local dap = require("dap")
-
-dap.adapters.gdb = {
-	type = "executable",
-	command = "gdb",
-	args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+dap.adapters.cppdbg = {
+    type = "executable",
+    command = "gdb",
+    args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
 }
+
 dap.configurations.c = {
-	{
-		name = "Launch",
-		type = "gdb",
-		request = "launch",
-		program = function()
-			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-		end,
-		args = {}, -- provide arguments if needed
-		cwd = "${workspaceFolder}",
-		stopAtBeginningOfMainSubprogram = false,
-	},
-	{
-		name = "Select and attach to process",
-		type = "gdb",
-		request = "attach",
-		program = function()
-			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-		end,
-		pid = function()
-			local name = vim.fn.input('Executable name (filter): ')
-			return require("dap.utils").pick_process({ filter = name })
-		end,
-		cwd = '${workspaceFolder}'
-	},
-	{
-		name = 'Attach to gdbserver :1234',
-		type = 'gdb',
-		request = 'attach',
-		target = 'localhost:1234',
-		program = function()
-			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-		end,
-		cwd = '${workspaceFolder}'
-	}
+    -- Existing Launch / attach configs
+    {
+        name = "Launch",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        args = {},
+        cwd = "${workspaceFolder}",
+        stopAtBeginningOfMainSubprogram = false,
+    },
+    {
+        name = "Select and attach to process",
+        type = "cppdbg",
+        request = "attach",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        pid = function()
+            local name = vim.fn.input('Executable name (filter): ')
+            return require("dap.utils").pick_process({ filter = name })
+        end,
+        cwd = '${workspaceFolder}'
+    },
+    {
+        name = 'Attach to gdbserver :1234',
+        type = 'cppdbg',
+        request = 'attach',
+        target = 'localhost:1234',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}'
+    },
+
+    -- New QEMU Debug SO3 config
+    {
+        name = "QEMU Debug SO3",
+        type = "cppdbg",
+        request = "attach",
+        program = "${workspaceFolder}/so3/so3",
+        cwd = "${workspaceFolder}",
+        miDebuggerPath = "gdb",
+        miDebuggerServerAddress = "localhost:1234",
+		target = "localhost:1234",
+        preLaunchTask = function()
+            -- 1. Build kernel
+            vim.fn.system("make so3")
+
+            -- 2. Start QEMU if not already running
+            -- Note: jobstart will run it in the background
+            vim.fn.jobstart("./st -S", {detach = true})
+        end,
+        setupCommands = {
+            {
+                description = "Enable pretty-printing for gdb",
+                text = "-enable-pretty-printing",
+                ignoreFailures = true
+            },
+            {
+                description = "Set source directories",
+                text = "directory ${workspaceFolder}/so3",
+                ignoreFailures = true
+            }
+        }
+    }
 }
 
 dap.configurations.cpp = dap.configurations.c
@@ -77,10 +105,8 @@ dap.configurations.rust = dap.configurations.c
 
 vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
 vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
-
--- Eval var under cursor
 vim.keymap.set("n", "<space>?", function()
-	require("dapui").eval(nil, { enter = true })
+    require("dapui").eval(nil, { enter = true })
 end)
 
 vim.keymap.set("n", "<F1>", dap.continue)
@@ -90,15 +116,7 @@ vim.keymap.set("n", "<F4>", dap.step_out)
 vim.keymap.set("n", "<F5>", dap.step_back)
 vim.keymap.set("n", "<F13>", dap.restart)
 
-dap.listeners.before.attach.dapui_config = function()
-	ui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-	ui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-	ui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-	ui.close()
-end
+dap.listeners.before.attach.dapui_config = function() ui.open() end
+dap.listeners.before.launch.dapui_config = function() ui.open() end
+dap.listeners.before.event_terminated.dapui_config = function() ui.close() end
+dap.listeners.before.event_exited.dapui_config = function() ui.close() end
